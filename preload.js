9157c1fdd2166c10911272d2b5cfcef218e6caee
Philip Spelman <philip.spelman@vaporstream.com>
2020-12-07 13:24:45 -0500

    - added a few new things - noticed the debugger started working all of a sudden

--- preload.js
+++ preload.js
@@ -1,6 +1,34 @@
-const {ipcRenderer} = require('electron')
+const {ipcRenderer, contextBridge} = require('electron')
 
 console.log(`preload functions to expose`, )
+
+// add the name of the API that I want to expose
+contextBridge.exposeInMainWorld('e_notification', {
+  sendNotification(message) {
+    ipcRenderer.send('notify', message)
+  }
+
+})
+
+
+// Note: another way to expose specific methods to the renderer
+contextBridge.exposeInMainWorld('electron', {
+  notificationApi: {
+    sendNotification(message) {
+      ipcRenderer.send('battery-status', batteryStatus)
+    }
+  },
+  batteryApi: {
+    sendBatteryStatus(batteryStatus) {
+      ipcRenderer.send('battery-status', batteryStatus)
+    }
+  },
+  fileApi: {
+    // files and stuff
+  }
+
+
+})
 
 window.sendNotification = (message) => {
   ipcRenderer.send('notify', message)
